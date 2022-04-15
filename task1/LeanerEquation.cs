4f627355a88541cae6a0233f68b6fdcb4fe1b42c
Pavel Bobylev <paha_ska_15@mail.ru>
2022-04-15 11:33:31 +0300

    Добавьте файлы проекта.

---
+++ task1/LeanerEquation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    struct LeanerEquation
+    {
+        double k;
+        double b;
+
+        public LeanerEquation (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0 && b == 0)
+            {
+                return "Решением является любое число";
+            }
+                
+            else if (k == 0)
+            {
+
+                return "Уравнение не имеет решения!";
+            }
+             else
+            {
+                double x = -b / k;
+                return $"x = {x}";
+            }
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Расчёт линейного уравнения:");
+            Console.Write("Введите k: ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            LeanerEquation leanerEquation = new LeanerEquation(k, b);
+            Console.Write(leanerEquation.Root());
+            Console.ReadLine();
+
+        }
+    }
+}
